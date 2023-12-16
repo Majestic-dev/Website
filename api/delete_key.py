c357789ed491b40a4db219430d5f359441f10f9f
Aron-Mathias <saararonmathias1@gmail.com>
2023-12-16 13:42:23 +0200

    Add delete_key API endpoint

---
+++ api/delete_key.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, request
+
+from database import DataManager
+
+delete_key_blueprint = Blueprint("delete_key", __name__)
+
+@delete_key_blueprint.route("/api/delete_key", methods=["DELETE"])
+async def delete_key():
+    auth_header = request.headers.get("Authorization")
+    if auth_header:
+        if await DataManager.check_key(auth_header):
+            await DataManager.delete_auth_key(auth_header)
+            return jsonify({"success": "Key deleted"}), 202
+        else:
+            return jsonify({"error": "Invalid key"}), 401
+    else:
+        return jsonify({"error": "No Authorization header"}), 400
