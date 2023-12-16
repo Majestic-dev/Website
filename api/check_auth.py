e1826d0791c9edaaefcd5b9485acbfdeb63eb278
Aron-Mathias <saararonmathias1@gmail.com>
2023-12-16 13:42:20 +0200

    Add check_auth endpoint to API

---
+++ api/check_auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify, request
+
+from database import DataManager
+
+check_auth_blueprint = Blueprint("check_auth", __name__)
+
+@check_auth_blueprint.route("/api/check_auth", methods=["POST"])
+async def check_auth():
+    auth_header = request.headers.get("Authorization")
+    if auth_header:
+        if await DataManager.check_key(auth_header):
+            return jsonify({"success": "You are authenticated"}), 202
+        else:
+            return jsonify({"error": "Invalid key"}), 401
+    else:
+        return jsonify({"error": "No Authorization header"}), 400
