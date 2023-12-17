ebfe1dccf7c2d05cc86e5b0e72baa0748747fff2
Aron-Mathias <saararonmathias1@gmail.com>
2023-12-17 22:29:38 +0200

    Refactor app.py to use nested blueprints

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from api.get_key import get_key_blueprint
 from api.check_auth import check_auth_blueprint
 from api.delete_key import delete_key_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(get_key_blueprint)
-app.register_blueprint(check_auth_blueprint)
-app.register_blueprint(delete_key_blueprint)
+
+api = Blueprint("api", __name__, url_prefix="/api")
+
+api.register_blueprint(get_key_blueprint)
+api.register_blueprint(check_auth_blueprint)
+api.register_blueprint(delete_key_blueprint)
+
+app.register_blueprint(api)
 
 if __name__ == "__main__":
     app.run(debug=True)
