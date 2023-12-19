5e47f6901249f05c123413f7fc109302ecf92567
Aron-Mathias <saararonmathias1@gmail.com>
2023-12-19 21:05:33 +0200

    Add send_webhook blueprint to app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, Blueprint
 from api.get_key import get_key_blueprint
 from api.check_auth import check_auth_blueprint
 from api.delete_key import delete_key_blueprint
+from api.send_webhook import send_webhook_blueprint
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ api = Blueprint("api", __name__, url_prefix="/api")
 api.register_blueprint(get_key_blueprint)
 api.register_blueprint(check_auth_blueprint)
 api.register_blueprint(delete_key_blueprint)
+api.register_blueprint(send_webhook_blueprint)
 
 app.register_blueprint(api)
 
