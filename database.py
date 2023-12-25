cbdecccd4ca4a7a059bc636620b68a3b98e197cc
Aron-Mathias <saararonmathias1@gmail.com>
2023-12-25 20:27:46 +0200

    Refactor user authentication and add authentication key

--- database.py
+++ database.py
@@ -9,126 +9,117 @@ class DataManager:
         db = await asqlite.connect("data/data.db")
         cursor = await db.cursor()
         await cursor.execute(
-            """CREATE TABLE IF NOT EXISTS auth_keys (
-                key TEXT PRIMARY KEY,
-                user TEXT
-            )"""
-        )
-        await cursor.execute(
             """CREATE TABLE IF NOT EXISTS users (
                 user TEXT PRIMARY KEY,
-                password TEXT
+                password TEXT,
+                authentication_key TEXT
             )"""
         )
         await db.commit()
         await db.close()
 
     @classmethod
-    async def add_column(cls, column_name, column_type) -> None:
+    async def add_column(cls, column_name: str, column_type: str) -> None:
         db = await asqlite.connect("data/data.db")
         cursor = await db.cursor()
         await cursor.execute(
-            f"""ALTER TABLE auth_keys ADD COLUMN {column_name} {column_type}"""
+            f"""ALTER TABLE users ADD COLUMN {column_name} {column_type}"""
         )
         await db.commit()
         await db.close()
 
     @classmethod
-    async def create_auth_key(cls, user) -> None:
-        key = secrets.token_urlsafe(16)
-        db = await asqlite.connect("data/data.db")
-        cursor = await db.cursor()
-        await cursor.execute(
-            """INSERT INTO auth_keys (key, user) VALUES (?, ?)""", (key, user)
-        )
-        await db.commit()
-        return key
-
-    @classmethod
-    async def delete_auth_key(cls, key) -> None:
-        db = await asqlite.connect("data/data.db")
-        cursor = await db.cursor()
-        await cursor.execute("""DELETE FROM auth_keys WHERE key=?""", (key,))
-        await db.commit()
-        await db.close()
-
-    @classmethod
-    async def check_user_from_keys(cls, user) -> None:
-        db = await asqlite.connect("data/data.db")
-        cursor = await db.cursor()
-        await cursor.execute("""SELECT * FROM auth_keys WHERE user=?""", (user,))
-        result = await cursor.fetchone()
-        await db.close()
-        if result is None:
-            return False
-        else:
-            return True
-        
-    @classmethod
-    async def check_user_from_users(cls, user) -> None:
+    async def check_if_user_exists(cls, user: str) -> bool:
         db = await asqlite.connect("data/data.db")
         cursor = await db.cursor()
         await cursor.execute("""SELECT * FROM users WHERE user=?""", (user,))
         result = await cursor.fetchone()
         await db.close()
         if result is None:
-            return False
+            return False # User does not exist
         else:
-            return True
+            return True # User exists
 
     @classmethod
-    async def get_key(cls, user) -> None:
+    async def register_user(cls, user: str, password: str) -> None:
         db = await asqlite.connect("data/data.db")
         cursor = await db.cursor()
-        await cursor.execute("""SELECT * FROM auth_keys WHERE user=?""", (user,))
+        if await cls.check_if_user_exists(user):
+            return False # User already exists
+        else:
+            await cursor.execute(
+                """INSERT INTO users (user, password) VALUES (?, ?)""", (user, password)
+            )
+            await db.commit()
+            await db.close()
+            return True # User registered
+        
+    @classmethod
+    async def login_user(cls, user: str, password: str) -> None:
+        db = await asqlite.connect("data/data.db")
+        cursor = await db.cursor()
+        await cursor.execute("""SELECT * FROM users WHERE user=? AND password=?""", (user, password))
+        result = await cursor.fetchone()
+        await db.close()
+        if result is None:
+            return False # Account details are incorrect
+        else:
+            return True # Account details are correct
+
+    @classmethod
+    async def create_authentication_key(cls, user: str, password: str) -> None:
+        db = await asqlite.connect("data/data.db")
+        cursor = await db.cursor()
+        key = secrets.token_hex(16)
+
+        if await DataManager.check_if_user_exists(user):
+            if await DataManager.login_user(user, password):
+                await cursor.execute(
+                    """UPDATE users SET authentication_key = ? WHERE user = ? AND password = ?""", (key, user, password)
+                )
+                await db.commit()
+                await db.close()
+                return key
+            else:
+                return None
+        else:
+            return None
+
+    @classmethod
+    async def delete_authentication_key(cls, key) -> None:
+        db = await asqlite.connect("data/data.db")
+        cursor = await db.cursor()
+        await cursor.execute("""DELETE FROM users WHERE key=?""", (key,))
+        await db.commit()
+        await db.close()
+
+    @classmethod
+    async def get_authentication_key(cls, user) -> None:
+        db = await asqlite.connect("data/data.db")
+        cursor = await db.cursor()
+        await cursor.execute("""SELECT authentication_key FROM users WHERE user=?""", (user,))
         result = await cursor.fetchone()
         await db.close()
         if result is None:
             return None
         else:
-            return result[0]
+            return result[0]  # Extract the value from the Row object
 
     @classmethod
     async def check_key(cls, key) -> None:
         db = await asqlite.connect("data/data.db")
         cursor = await db.cursor()
-        await cursor.execute("""SELECT * FROM auth_keys WHERE key=?""", (key,))
+        await cursor.execute("""SELECT * FROM users WHERE key=?""", (key,))
         result = await cursor.fetchone()
         await db.close()
         if result is None:
-            return False
+            return False # Key does not exist
         else:
-            return True
-        
-    @classmethod
-    async def sign_up(cls, user, password) -> None:
-        db = await asqlite.connect("data/data.db")
-        cursor = await db.cursor()
-        if await cls.check_user_from_users(user):
-            return False
-        else:
-            await cursor.execute("""INSERT INTO users (user, password) VALUES (?, ?)""", (user, password))
-            await db.commit()
-            await db.close()
-            return True
-
-    @classmethod
-    async def log_in(cls, user, password) -> None:
-        db = await asqlite.connect("data/data.db")
-        cursor = await db.cursor()
-        await cursor.execute("""SELECT * FROM users WHERE user=?""", (user,))
-        result = await cursor.fetchone()
-        await db.close()
-        if result is None:
-            return False
-        else:
-            if result[1] == password:
-                return True
-            else:
-                return False
+            return True # Key exists
 
 async def main():
     await DataManager.initialise()
+    await DataManager.create_authentication_key("test", "test")
 
 if __name__ == "__main__":
     asyncio.run(main())
